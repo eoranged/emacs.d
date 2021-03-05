860f4c84a18f4486ece42aefb5591f03959f01b9
Vladimir Protasov <github@eoranged.com>
2021-03-06 00:09:23 +0300

    Initial config
    Cudos to aragaer for basic structure and to tarsius for theme.

---
+++ init.el
@@ -0,0 +1,26 @@
+(defconst old-gc-cons-threshold gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook
+ 'emacs-startup-hook
+ (lambda ()
+   (let ((startup-time (float-time (time-subtract after-init-time before-init-time))))
+     (message "Emacs ready in %.2f seconds with %d garbage collections." startup-time gcs-done))
+   (setq gc-cons-threshold old-gc-cons-threshold)))
+
+(require 'package)
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "https://marmalade-repo.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/")))
+
+(package-initialize)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name (locate-user-emacs-file "myinit.org")))
+
